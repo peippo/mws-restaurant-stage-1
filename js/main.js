04119cad1554210c06010d3da719308a423941e1
Janne Peippo <peippo@lumous.com>
2018-06-07 20:20:09 +0300

    Use aria-label for more descriptive link purpose

--- js/main.js
+++ js/main.js
@@ -162,6 +162,7 @@ createRestaurantHTML = (restaurant) => {
 	const more = document.createElement('a');
 	more.innerHTML = 'View Details';
 	more.href = DBHelper.urlForRestaurant(restaurant);
+	more.setAttribute('aria-label', `View ${restaurant.name} details`);
 	infoWrapper.append(more);
 
 	li.append(infoWrapper);
