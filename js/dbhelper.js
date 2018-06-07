f08c70252d04a62fd5f7239813aaa4f1937bc75c
Janne Peippo <peippo@lumous.com>
2018-06-07 21:33:50 +0300

    Change database URL forming function
    Remove hard coded localhost URL and instead get the hostname from window.location

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
 	 * Change this to restaurants.json file location on your server.
 	 */
 	static get DATABASE_URL() {
-		const port = 3000; // Change this to your server port
-		return `http://localhost:${port}/data/restaurants.json`;
+		//const port = 3000; // Change this to your server port
+		//return `http://localhost:${port}/data/restaurants.json`;
+		return `${window.location.protocol}//${window.location.host}/data/restaurants.json`;
 	}
 
 	/**
