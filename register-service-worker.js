81792792ec8830ff920ff71c51081853b3d5e872
Janne Peippo <peippo@lumous.com>
2018-06-10 16:25:46 +0300

    Adjust service worker

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,11 +1,5 @@
-// Cache-first network service worker, built at https://www.pwabuilder.com/
-
-if (navigator.serviceWorker.controller) {
-	console.log('Active service worker found, no need to register');
-} else {
-	navigator.serviceWorker.register('service-worker.js', {
-		scope: './'
-	}).then(function(reg) {
-		console.log('Service worker registered for scope:'+ reg.scope);
-	});
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('service-worker.js')
+		.then(function() { console.log('Service worker registered'); });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,28 @@
-// "Offline copy of pages" service worker, built at https://www.pwabuilder.com/
-
-self.addEventListener('install', function(event) {
-	var indexPage = new Request('index.html');
-	event.waitUntil(
-		fetch(indexPage).then(function(response) {
-			return caches.open('sw-offline').then(function(cache) {
-				console.log('Cached index page during install '+ response.url);
-				return cache.put(indexPage, response);
-			});
-	}));
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('mws-restaurant').then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/restaurant.html?id=1',
+				'/restaurant.html?id=2',
+				'/restaurant.html?id=3',
+				'/restaurant.html?id=4',
+				'/restaurant.html?id=5',
+				'/restaurant.html?id=6',
+				'/restaurant.html?id=7',
+				'/restaurant.html?id=8',
+				'/restaurant.html?id=9',
+				'/restaurant.html?id=10'
+			]);
+		})
+	);
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function(event) {
-	var updateCache = function(request){
-		return caches.open('sw-offline').then(function (cache) {
-			return fetch(request).then(function (response) {
-				console.log('Add page to offline '+response.url);
-				return cache.put(request, response);
-			});
-		});
-	};
-
-	event.waitUntil(updateCache(event.request));
-
 	event.respondWith(
-		fetch(event.request).catch(function(error) {
-			console.log( 'Network request Failed. Serving content from cache: ' + error );
-
-			//Check to see if you have it in the cache
-			//Return response
-			//If not in the cache, then return error page
-			return caches.open('sw-offline').then(function (cache) {
-				return cache.match(event.request).then(function (matching) {
-					var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-					return report;
-				});
-			});
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
 		})
 	);
 });
