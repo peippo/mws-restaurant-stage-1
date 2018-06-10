95a8c857c1d38db51793aea1ed07b2a72d024141
Janne Peippo <peippo@lumous.com>
2018-06-10 16:42:17 +0300

    Update Service worker cache paths

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,18 @@ self.addEventListener('install', function(e) {
 	e.waitUntil(
 		caches.open('mws-restaurant').then(function(cache) {
 			return cache.addAll([
-				'/',
-				'/index.html',
-				'/restaurant.html?id=1',
-				'/restaurant.html?id=2',
-				'/restaurant.html?id=3',
-				'/restaurant.html?id=4',
-				'/restaurant.html?id=5',
-				'/restaurant.html?id=6',
-				'/restaurant.html?id=7',
-				'/restaurant.html?id=8',
-				'/restaurant.html?id=9',
-				'/restaurant.html?id=10'
+				'./',
+				'./index.html',
+				'./restaurant.html?id=1',
+				'./restaurant.html?id=2',
+				'./restaurant.html?id=3',
+				'./restaurant.html?id=4',
+				'./restaurant.html?id=5',
+				'./restaurant.html?id=6',
+				'./restaurant.html?id=7',
+				'./restaurant.html?id=8',
+				'./restaurant.html?id=9',
+				'./restaurant.html?id=10'
 			]);
 		})
 	);
